2de8bf634d2f997f5340945f675ef5427a417daf
Mathieu Cuvelier <mathieu.cuvelier-ext@chantiers-atlantique.com>
2022-09-27 08:33:06 +0200

    fix title ; add test project

---
+++ Preteur.ServicesTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Preteur.ServicesTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
